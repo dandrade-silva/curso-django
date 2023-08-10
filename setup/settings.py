48f762a29b3ee9a57a3a70827f0384134635dff3
Danilo Andrade <dandrade0803@gmail.com>
2023-08-09 21:05:42 -0300

    Configuração dos arquivos estáticos

--- setup/settings.py
+++ setup/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = bool(os.getenv('DEBUG'))
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'danilo-andrade.dev.br']
 
@@ -121,6 +121,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'setup/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
